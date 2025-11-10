f606c64baf6f92ab63fcfff8bc03a8913ca38d40
karan-leapwallet <112631127+karan-leapwallet@users.noreply.github.com>
2025-11-10 18:03:44 +0530

    Time: 87 ms (16.80%) | Memory: 65 MB (81.84%) - LeetSync

---
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+    nums = nums.sort((a, b) => Number(a) - Number(b));
+    return nums.at(-k);
+};
